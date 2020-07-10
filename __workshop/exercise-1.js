5e83791537a2cde43ad667f8a5fae33987d51124
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-07-10 10:43:56 -0400

    Solution to exercise 1

--- __workshop/exercise-1.js
+++ __workshop/exercise-1.js
@@ -4,11 +4,27 @@ const arrayOfWords = ['cucumber', 'tomatos', 'avocado'];
 const complicatedArray = ['cucumber', 44, true];
 
 const makeAllCaps = (array) => {
-  // write some code
+  myPromise = new Promise((resolve, reject) => {
+    if (array.every(element => typeof element == 'string')) {
+      const capitalized = array.map(word => word.charAt(0).toUpperCase() + word.substr(1));
+      resolve(capitalized);
+    } else {
+      reject('Array must only be comprised of strings');
+    }
+  });
+  return myPromise;
 };
 
 const sortWords = (array) => {
-  // write some code
+  myPromise = new Promise((resolve, reject) => {
+    const sorted = array.sort((a, b) => {
+      if (a > b) return 1;
+      else if (a < b) return -1;
+      else return 0;
+    });
+    resolve(sorted);
+  });
+  return myPromise
 };
 
 // Calling (testing)
