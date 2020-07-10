6007444393890595da80050ad7a85d797a794258
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-07-10 11:01:34 -0400

    Solution to exercise 2.3

--- __workshop/exercise-2.3.js
+++ __workshop/exercise-2.3.js
@@ -4,4 +4,17 @@
 // require the 'request-promise' module.
 const request = require('request-promise');
 
-// get the code you wrote in 2.2 and paste it here...
+const greeting = (langCode) => {
+  return request(`https://journeyedu.herokuapp.com/hello/${langCode}`) // 1
+    .then((response) => JSON.parse(response))
+    .then((parsedResponse) => {
+      return parsedResponse.data.text; // 2
+    })
+    .catch((err) => err.error ? JSON.parse(err.error) : err);
+};
+
+// Testing
+greeting('fr').then((result) => console.log(result)); // { lang: "French", code: "FR", text: "Bonjour" }
+greeting('es').then((result) => console.log(result));
+greeting('ru').then((result) => console.log(result));
+greeting('blah').then((result) => console.log(result));
